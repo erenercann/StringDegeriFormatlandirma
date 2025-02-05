db66b1da5f831f755702a825567d414456bec33e
e_ercan <e_ercan@CERIT10>
2025-02-05 11:51:10 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace string_turu
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        
+        {
+            #region String Formatlandırma
+            string isim = "Eren", soyisim = "Ercan", tcNo = "12345678901";
+            int yas = 30;
+            bool medeniHal = true;
+
+            Console.WriteLine("Tc No: "+ tcNo + " olan " + isim + " " + soyisim + " adlı şahsın bilgileri | Yaş: " + yas +" "+ "| Medeni Hal: " + medeniHal);
+            #endregion
+
+        }
+    }
+}
