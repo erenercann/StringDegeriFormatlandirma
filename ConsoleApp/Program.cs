01b3b8928563b1d05ca46b72b65b851434c69f2c
Eren Ercan <eren_ercan@ykk.com>
2025-02-05 15:53:26 +0300

    last version

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,7 +11,6 @@ namespace string_turu
             string isim = "Eren", soyisim = "Ercan", tcNo = "12345678901";
             int yas = 30;
             bool medeniHal = true;
-            Console.WriteLine("sonunda");
             Console.WriteLine("Tc No: "+ tcNo + " olan " + isim + " " + soyisim + " adlı şahsın bilgileri | Yaş: " + yas +" "+ "| Medeni Hal: " + medeniHal);
             #endregion
 
