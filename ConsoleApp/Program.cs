dc86beef78f918fa4bb1e99c330a90f7c36b0458
Eren Ercan <eren_ercan@ykk.com>
2025-02-06 10:24:50 +0300

    string.Format Metodu İle Formatlandırma

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,7 +11,13 @@ namespace string_turu
             string isim = "Eren", soyisim = "Ercan", tcNo = "12345678901";
             int yas = 30;
             bool medeniHal = true;
-            Console.WriteLine("Tc No: "+ tcNo + " olan " + isim + " " + soyisim + " adlı şahsın bilgileri | Yaş: " + yas +" "+ "| Medeni Hal: " + medeniHal);
+          //Console.WriteLine("Tc No: " + tcNo + " olan " + isim + " " + soyisim + " adlı şahsın bilgileri | Yaş: " + yas + " " + "| Medeni Hal: " + (medeniHal ? "Evli" : "Bekar"));
+            #endregion
+
+            #region String Değeri string.Format Metodu İle Formatlandırma
+
+            string sonuc = string.Format("Tc No: {0} olan {1} {2} adlı şahsın bilgileri | Yaş: {3} | Medeni Hal: {4}",tcNo,isim,soyisim,yas,medeniHal ? "Evli" : "Bekar");
+            Console.WriteLine(sonuc);
             #endregion
 
         }
